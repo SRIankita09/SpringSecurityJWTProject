e08de3013120139a99e81681a40b392bef9d03e2
Ankita Srivastava <ankita.sr009@gmail.com>
2024-10-06 09:08:18 +0100

    Add files via upload

---
+++ src/test/java/Spring/Security/projectJWT/ProjectJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package Spring.Security.projectJWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
