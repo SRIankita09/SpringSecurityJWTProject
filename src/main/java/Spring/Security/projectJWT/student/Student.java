5f713f421708ff45d99492f6768d2f7d1266d306
Ankita Srivastava <ankita.sr009@gmail.com>
2024-10-06 11:28:33 +0100

    bootstrap

---
+++ src/main/java/Spring/Security/projectJWT/student/Student.java
@@ -0,0 +1,22 @@
+package Spring.Security.projectJWT.student;
+
+public class Student {
+
+    private  final Integer studentId;
+    private final String studentName;
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public Integer getStudentId() {
+        return studentId;
+    }
+
+    public Student(Integer studentId, String studentName) {
+        this.studentId = studentId;
+        this.studentName = studentName;
+    }
+
+
+}
